8aec3d7201f27c83d2dd1a87887dc853ac8b6fe2
Vitor Gois <vitorcgois777@gmail.com>
2020-11-20 18:43:02 -0300

    Created CRUD - Client

--- src/main/java/com/api/rentalcars/repository/ClientRepository.java
+++ src/main/java/com/api/rentalcars/repository/ClientRepository.java
@@ -1,8 +1,39 @@
 package com.api.rentalcars.repository;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import com.api.rentalcars.model.Client;
+
 import org.springframework.stereotype.Component;
 
 @Component
 public class ClientRepository {
+
+	private ArrayList<Client> clients = new ArrayList<>();
+
+	private int nextID = 1;
+
+	public List<Client> getAllClients() {
+		return clients;
+	}
+
+	public Optional<Client> getClientByID(int id) {
+		for(Client aux : clients) {
+			if(aux.getCode() == id) {
+				return Optional.of(aux);
+			}
+		}
+
+		return Optional.empty();
+	}
+
+	public Client save(Client newClient) {
+		newClient.setCode(nextID++);
+		clients.add(newClient);
+
+		return newClient;
+	}
     
 }
